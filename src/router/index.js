21846ce09e62895bbe3177c64a803a691c513365
lily <13516138165@163.com>
2020-03-03 18:44:44 +0800

    新建登录和主页

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,23 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
-
+import Home from '../views/home'
+import Login from '../views/login'
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'home',
     component: Home
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
   }
   // {
   //   path: '/about',
