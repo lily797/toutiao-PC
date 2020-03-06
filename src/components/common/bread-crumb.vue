7f6c113085d5e585bf5084346ae2ffc8a397e81e
lily <13516138165@163.com>
2020-03-07 01:07:47 +0800

    面包屑封装

---
+++ src/components/common/bread-crumb.vue
@@ -0,0 +1,15 @@
+<template>
+  <el-breadcrumb separator=">">
+    <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+    <el-breadcrumb-item>
+        <!-- 具名插槽占坑 -->
+        <slot name="title"></slot>
+    </el-breadcrumb-item>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,11 @@
 
 import layoutAside from '../components/home/layout-aside.vue'
 import layoutHeader from '../components/home/layout-header.vue'
-
+import breadCrumb from '../components/common/bread-crumb.vue'
 export default {
   install (Vue) {
     Vue.component('layout-aside', layoutAside)
     Vue.component('layout-header', layoutHeader)
+    Vue.component('bread-crumb'), breadCrumb
   }
 }
