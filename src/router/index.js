d85f3299445457a8392e78f17507e388e2c04c56
lily <13516138165@163.com>
2020-03-06 21:18:08 +0800

    home页的二级路由-默认

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/home'
 import Login from '../views/login'
+import Main from '../views/home/main'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,15 +10,21 @@ const routes = [
     path: '/',
     redirect: '/home'
   },
-  {
-    path: '/home',
-    name: 'home',
-    component: Home
-  },
+
   {
     path: '/login',
     name: 'login',
     component: Login
+  },
+  {
+    path: '/home',
+    name: 'home',
+    component: Home,
+    children: [{
+      path: '',
+      component: Main
+    }]
+
   }
   // {
   //   path: '/about',
