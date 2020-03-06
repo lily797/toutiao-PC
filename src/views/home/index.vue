4e1a113272261e385b00ab00d2ecac9bcf420293
lily <13516138165@163.com>
2020-03-06 10:04:08 +0800

    主页布局

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,15 +1,16 @@
 <template>
-  <div class="home">
-    home
-  </div>
+  <el-container>
+    <el-aside>侧边栏</el-aside>
+    <el-container>
+      <el-header>上</el-header>
+      <el-main>中</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-
 export default {
-  name: 'Home',
-  components: {
-
-  }
+  // name: "Home",
+  components: {}
 }
 </script>
