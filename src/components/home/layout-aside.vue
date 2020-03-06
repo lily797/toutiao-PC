913ebb077e9daa784a841b29fa7a1621ea2ad319
lily <13516138165@163.com>
2020-03-06 13:28:08 +0800

    主页模块-左侧导航菜单

---
+++ src/components/home/layout-aside.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="layout-aside">
+    <div class="title">
+      <img src="../../assets/img/logo_admin.png" alt />
+    </div>
+    <el-menu
+      default-active="2"
+      class="el-menu-vertical-demo"
+      @open="handleOpen"
+      @close="handleClose"
+      background-color="#353e4b"
+      text-color="#adafb5"
+      active-text-color="#ffd04b"
+    >
+      <el-menu-item>
+        <i class="el-icon-s-home"></i>
+        <span slot="title">首页</span>
+      </el-menu-item>
+
+      <el-submenu>
+        <template slot="title">
+          <i class="el-icon-s-grid"></i>
+          <span>内容管理</span>
+        </template>
+        <el-menu-item>
+          <span slot="title">发表文章</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">内容列表</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">评论列表</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">素材管理</span>
+        </el-menu-item>
+      </el-submenu>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-document-copy"></i>
+          <span>粉丝管理</span>
+        </template>
+        <el-menu-item>
+          <span slot="title">图文数据</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">粉丝概况</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">粉丝画像</span>
+        </el-menu-item>
+        <el-menu-item>
+          <span slot="title">粉丝列表</span>
+        </el-menu-item>
+      </el-submenu>
+
+      <el-menu-item>
+        <i class="el-icon-user-solid"></i>
+        <span slot="title">账户管理</span>
+      </el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="less" scoped>
+.layout-aside {
+  .title {
+    padding: 15px 0;
+    text-align: center;
+    background-color: #2e2f32;
+    img {
+      width: 120px;
+      height: 30px;
+    }
+  }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,8 @@
 <template>
   <el-container>
-    <el-aside>侧边栏</el-aside>
+    <el-aside style="width:200px;height:100vh;background:#323745">
+      <layout-aside></layout-aside>
+    </el-aside>
     <el-container>
       <el-header>上</el-header>
       <el-main>中</el-main>
@@ -9,8 +11,11 @@
 </template>
 
 <script>
+import layoutAside from '../../components/home/layout-aside'
 export default {
   // name: "Home",
-  components: {}
+  components: {
+    'layout-aside': layoutAside
+  }
 }
 </script>
